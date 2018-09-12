b189b6b9d358ff4bbbbe9fef2442b302c997cf5e
Christopher Tung <cstung21@gmail.com>
2018-09-12 14:51:20 -0700

    updated query to grab all data, configured webpack and airbnb linter, defined previously undefined database function getBusinessPhotos

--- database/index.js
+++ database/index.js
@@ -1,23 +1,25 @@
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'yelpest'
+  database: 'yelpest',
 });
 
-getBusinessPhotos = (busId, callback) => {
-  connection.query('SELECT * FROM photos WHERE bus_id = ?', [busId], (error, results) => {
+const getBusinessPhotos = (busId, callback) => {
+  const query = 'SELECT photos.id, photos.bus_id, photos.user_id, photos.description, photos.link, photos.date, photos.helpful, photos.not_helpful, users.name AS user_name, users.is_owner, users.friends, users.reviews, users.elite_status, businesses.name AS bus_name, businesses.takes_reservation FROM photos INNER JOIN users ON photos.user_id = users.id INNER JOIN businesses ON photos.bus_id = businesses.id WHERE bus_id = ?';
+  connection.query(query, [busId], (error, results) => {
     if (error) {
       console.log(`ERROR on database querying photos for business ID ${busId}`);
       callback(error);
     } else {
       console.log(`Successfully queried photos for business ID ${busId} on database!`);
-      callback(null, results)
+      callback(null, results);
     }
   });
 };
 
 module.exports = {
-  connection: connection,
-  getBusinessPhotos: getBusinessPhotos
+  connection,
+  getBusinessPhotos,
 };
