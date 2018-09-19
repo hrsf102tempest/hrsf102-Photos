64e3c83ce74770e65cb48ca45f8371823da978de
Christopher Tung <cstung21@gmail.com>
2018-09-19 10:33:56 -0700

    update server route path for dynamic data rendering based on business ID specified in URL

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const db = require('../database/index.js');
 const app = express();
 const PORT = 3002;
 
-app.use('/biz/:id', express.static(path.join(__dirname, '/../client/dist')));
+app.use('/biz/', express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/businesses/:busId', (req, res) => {
   console.log(`Received request from client for business ID ${req.params.busId}`);
