8b91a530e2ee225eb534e15d4f0d57e6a67a428b
Christopher Tung <cstung21@gmail.com>
2018-09-19 12:25:11 -0700

    enable CORS

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const db = require('../database/index.js');
 const app = express();
 const PORT = 3002;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/biz/', express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/businesses/:busId', (req, res) => {
